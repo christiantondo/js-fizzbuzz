939976b17362788f70d002a66f85eebc4e0daa8c
christiantondo <christiantondo1@gmail.com>
2025-11-01 20:13:17 +0100

    ciclo completo

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 console.log("JS on")
 
+for (let i = 1; i <= 100; i++) {
+
+    let contenuto = i;
+
+    if (i % 15 == 0) {
+        contenuto = `FizzBuzz`;
+
+    } else if (i % 3 == 0) {
+        contenuto = `Fizz`;
+
+    } else if (i % 5 == 0) {
+        contenuto = `Buzz`;
+        
+    }
+
+}
+
+console.log("Ciclo terminato");
